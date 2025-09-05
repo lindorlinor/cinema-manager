d85497b787dd01371f3aa7f6b129fcd6b560d318
angela <angela.canazza@studenti.unipd.it>
2025-09-05 23:36:37 +0200

    aiuto ma che succede

--- GUI/SearchPanel.h
+++ GUI/SearchPanel.h
@@ -63,7 +63,7 @@ class SearchPanel:public QWidget{
     int comboOrdinamento = 0; 
     QString filtroBottone = "Film"; 
     QString ricerca = "";
-    bool = 0; // 0 default, 1 espanso (sono le possibili viste)
+    /* bool = 0; */ // 0 default, 1 espanso (sono le possibili viste)
 
     //media che sto visualizzando
     MediaView* detailPage;
