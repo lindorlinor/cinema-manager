b88d8a40b4d9c6accb717d89bdc1e0574393ea98
angela <angela.canazza@studenti.unipd.it>
2025-09-08 11:23:41 +0200

    aggoiornato le date

--- GUI/CinemaButton.cpp
+++ GUI/CinemaButton.cpp
@@ -1,19 +1,19 @@
 #include "CinemaButton.h"
 
-CinemaButton::CinemaButton(const QString& cinemaName="Untitled", const QPixmap& cinemaImage, QWidget *parent)
+CinemaButton::CinemaButton(const QString &cinemaName = "Untitled", const QPixmap &cinemaImage, QWidget *parent)
     : QFrame(parent)
 {
-    setFixedSize(150, 180);  // larghezza x altezza
+    setFixedSize(150, 180); // larghezza x altezza
     setObjectName("CinemaButton");
     imageLabel = new QLabel(this);
     imageLabel->setPixmap(cinemaImage.scaled(120, 120));
     imageLabel->setAlignment(Qt::AlignCenter);
-    
-    nameLabel = new QLabel(cinemaName,this);
+
+    nameLabel = new QLabel(cinemaName, this);
     nameLabel->setAlignment(Qt::AlignCenter);
     nameLabel->setWordWrap(true);
 
-    QVBoxLayout* layout = new QVBoxLayout(this);
+    QVBoxLayout *layout = new QVBoxLayout(this);
     layout->addWidget(imageLabel);
     layout->addWidget(nameLabel);
     setLayout(layout);
@@ -25,8 +25,8 @@ CinemaButton::CinemaButton(const QString& cinemaName="Untitled", const QPixmap& 
 
 void CinemaButton::mousePressEvent(QMouseEvent *event)
 {
-    if (event->button() == Qt::LeftButton) {
+    if (event->button() == Qt::LeftButton)
+    {
         emit selected();
     }
 }
-
